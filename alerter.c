81c36ed9eb7996e2d9a65c1c00708786f794e69b
RajeshSR0809 <srrajesh.svpur@gmail.com>
2022-06-19 23:58:13 +0530

    alerter test cases updated

--- alerter.c
+++ alerter.c
@@ -3,18 +3,40 @@
 
 int alertFailureCount = 0;
 
-int networkAlertStub(float celcius) {
+enum environment
+{
+    Test,
+    Prod
+};
+
+int networkAlerter(enum environment env, float celcius){
+    if(env == 0) return networkAlertStub(celcius);
+    else return networkAlerterReal(celcius);
+}
+
+int networkAlerterReal(float celcius)
+{
+    // this method will connect with real devices and decides based on device and input temp
+    // for now returning 200
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
-    // Return 200 for ok
-    // Return 500 for not-ok
+    return 200;
+}
+
+int networkAlertStub(float celcius)
+{
+    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
+    // Return 200 for ok, ok is celcius < 800
+    // Return 500 for not-ok, not-ok is celcius >= 800
     // stub always succeeds and returns 200
     return 200;
 }
 
-void alertInCelcius(float farenheit) {
+void alertInCelcius(enum environment env, float farenheit)
+{
     float celcius = (farenheit - 32) * 5 / 9;
-    int returnCode = networkAlertStub(celcius);
-    if (returnCode != 200) {
+    int returnCode = networkAlerter(env,celcius);
+    if (returnCode != 200)
+    {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
@@ -23,9 +45,14 @@ void alertInCelcius(float farenheit) {
     }
 }
 
-int main() {
-    alertInCelcius(400.5);
-    alertInCelcius(303.6);
+int main()
+{
+    alertInCelcius(0, 400.5);
+    assert(alertFailureCount == 0);
+    alertInCelcius(0, 303.6);
+    assert(alertFailureCount == 0);
+    alertInCelcius(0, 1473.8);
+    assert(alertFailureCount == 1);
     printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
     return 0;
