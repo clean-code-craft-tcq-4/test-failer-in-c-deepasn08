4e138de7f744f0508e756f02e6b3279b64ebbb2f
RajeshSR0809 <srrajesh.svpur@gmail.com>
2022-06-20 00:01:16 +0530

    networkAlerter defined at later stage

--- alerter.c
+++ alerter.c
@@ -9,27 +9,27 @@ enum environment
     Prod
 };
 
-int networkAlerter(enum environment env, float celcius){
-    if(env == 0) return networkAlertStub(celcius);
-    else return networkAlerterReal(celcius);
-}
 
-int networkAlerterReal(float celcius)
-{
+int networkAlerterReal(float celcius){
     // this method will connect with real devices and decides based on device and input temp
     // for now returning 200
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
     return 200;
 }
 
-int networkAlertStub(float celcius)
-{
+int networkAlertStub(float celcius){
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
     // Return 200 for ok, ok is celcius < 800
     // Return 500 for not-ok, not-ok is celcius >= 800
     // stub always succeeds and returns 200
     return 200;
 }
+
+int networkAlerter(enum environment env, float celcius){
+    if(env == 0) return networkAlertStub(celcius);
+    else return networkAlerterReal(celcius);
+}
+
 
 void alertInCelcius(enum environment env, float farenheit)
 {
