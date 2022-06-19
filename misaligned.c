9c46068a8dadee1d5de6d623cf1d628baca2d24b
RajeshSR0809 <srrajesh.svpur@gmail.com>
2022-06-19 23:03:28 +0530

    updated the return type of printColorMap function

--- misaligned.c
+++ misaligned.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <assert.h>
 
-const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+const char *majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char *minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
 struct colorMap
 {
     int pairNumber;
-    char* majorColor;
-    char* minorColor;
+    char *majorColor;
+    char *minorColor;
 };
 
 const int numberOfValues = 25;
@@ -20,28 +20,30 @@ void getColorMap()
     {
         for (int j = 0; j < 5; j++)
         {
-           colorMapCollection[i * 5 + j].pairNumber = i * 5 + j;
-           colorMapCollection[i * 5 + j].majorColor = (char*)majorColor[i];
-           colorMapCollection[i * 5 + j].minorColor = (char*)minorColor[j];
-
+            colorMapCollection[i * 5 + j].pairNumber = i * 5 + j;
+            colorMapCollection[i * 5 + j].majorColor = (char *)majorColor[i];
+            colorMapCollection[i * 5 + j].minorColor = (char *)minorColor[j];
         }
     }
 }
 
-int printColorMap()
+void printColorMap()
 {
 
-    for(int i=0; i<numberOfValues; i++ ){
-    printf("%d | %s | %s\n", colorMapCollection[i].pairNumber, colorMapCollection[i].majorColor, colorMapCollection[i].minorColor);
+    for (int i = 0; i < numberOfValues; i++)
+    {
+        printf("%d | %s | %s\n", colorMapCollection[i].pairNumber, colorMapCollection[i].majorColor, colorMapCollection[i].minorColor);
     }
 }
 
-void Testfunction(){
-   for(int i = 0; i < 26; i++){
-       assert(colorMapCollection[i].pairNumber == i+1);
-       assert(colorMapCollection[i].majorColor == majorColor[i]);
-       assert(colorMapCollection[i].minorColor == minorColor[i]);
-   }
+void Testfunction()
+{
+    for (int i = 0; i < 26; i++)
+    {
+        assert(colorMapCollection[i].pairNumber == i + 1);
+        assert(colorMapCollection[i].majorColor == majorColor[i]);
+        assert(colorMapCollection[i].minorColor == minorColor[i]);
+    }
 }
 
 int main()
